c5229631fdba750c66ffcf936e933db6c70c660a
najimmy <najimmy@gmail.com>
2018-11-19 08:19:56 +0900

    test channel

---
+++ channel/close_broadcast.go
@@ -0,0 +1,28 @@
+//https://talks.golang.org/2012/10things.slide
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func waiter(i int, block, done chan struct{}) {
+	time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
+	fmt.Println(i, "waiting...")
+	<-block
+	fmt.Println(i, "done!")
+	done <- struct{}{}
+}
+
+func _main() {
+	block, done := make(chan struct{}), make(chan struct{})
+	for i := 0; i < 4; i++ {
+		go waiter(i, block, done)
+	}
+	time.Sleep(5 * time.Second)
+	close(block)
+	for i := 0; i < 4; i++ {
+		<-done
+	}
+}
